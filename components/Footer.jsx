f0215ca6217327804749114a47ef080ac54544e1
Shreyas Mulay Github <shreyasmulay23@gmail.com>
2025-04-01 06:20:04 +0530

    Polish.
    [no-story]

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,12 @@
 'use client';
 
-import { AiFillLinkedin, AiOutlineGithub } from 'react-icons/ai';
-import { LuMail } from 'react-icons/lu';
-
 export default function Footer() {
     return (
         <footer className="bg-primary text-white py-12">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="mt-8 pt-8 border-t border-gray-800 text-center text-gray-400">
-                    <p>© {new Date().getFullYear()} Shreyas Mulay. All rights reserved.</p>
+                    <p>© {new Date().getFullYear()} Shreyas Mulay. All rights reserved. | All images are property of
+                        their respective owners and are used with permission.</p>
                 </div>
             </div>
         </footer>
